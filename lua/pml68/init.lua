ec4d3696fad6c55932f924aa22c4009d4c2388a4
Polesznyák Márk László <116908301+pml68@users.noreply.github.com>
2024-03-17 23:00:50 +0100

    feat: missed the Float part

--- lua/pml68/init.lua
+++ lua/pml68/init.lua
@@ -2,7 +2,7 @@ require("pml68.remap")
 require("pml68.lazy")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.opt.relativenumber = true
 vim.cmd("set nu")
