1c33f6e015a3e2f433b62b5055202f3a1e843b69
pml68 <tutorialmester@gmail.com>
2024-04-01 23:54:41 +0200

    fix: MasonInstallAll command

--- lua/pml68/plugins.lua
+++ lua/pml68/plugins.lua
@@ -105,14 +105,14 @@ local plugins = {
     "williamboman/mason.nvim",
     cmd = {"Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate"},
     opts = function()
-      require("pml68.configs.mason")
+      return require("pml68.configs.mason")
     end,
     config = function(_, opts)
       require("mason").setup(opts)
       vim.api.nvim_create_user_command("MasonInstallAll", function()
-	if opts.ensure_installed and #opts.ensure_installed > 0 then
-	  vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
-	end
+				if opts.ensure_installed and #opts.ensure_installed > 0 then
+					vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+				end
       end, {})
 
       vim.g.mason_binaries_list = opts.ensure_installed
