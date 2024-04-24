468f486203b74e89edf40bd67c27b7f68ea38729
pml68 <pml31@protonmail.com>
2024-04-24 10:10:00 +0200

    feat: automatically install Mason extras on first run

--- README.md
+++ README.md
@@ -4,10 +4,8 @@ My custom Neovim setup
 
 # Installation
 
-Download in your config directory
+Download into your config directory
 
 ```sh
 git clone https://github.com/pml68/nvim-setup ~/.config/nvim
 ```
-
-After starting Neovim, and letting Lazy and nvim-treesitter install everything, run the `:MasonInstallAll` command to install the required LSPs and formatters
--- lua/pml68/plugins.lua
+++ lua/pml68/plugins.lua
@@ -123,12 +123,13 @@ local plugins = {
       require("mason").setup(opts)
       vim.api.nvim_create_user_command("MasonInstallAll",function()
         if opts.ensure_installed and #opts.ensure_installed > 0 then
-          vim.cmd("MasonInstall"..table.concat(opts.ensure_installed,""))
+          vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed," "))
         end
       end,{})
 
       vim.g.mason_binaries_list = opts.ensure_installed
-    end
+    end,
+    build = ":MasonInstallAll",
   },
   --Fuzzyfinder
   {
