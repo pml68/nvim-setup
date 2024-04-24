87e690301530b66b5ac995a8153630d9f08026e2
pml68 <pml31@protonmail.com>
2024-04-24 09:15:45 +0200

    style: fix indenting (hopefully)

--- lua/pml68/plugins.lua
+++ lua/pml68/plugins.lua
@@ -1,79 +1,79 @@
 local plugins = {
-  -- Colorizer
+  --Colorizer
   {
     "NvChad/nvim-colorizer.lua",
     event = "BufReadPost",
     config = function()
       require("colorizer").setup()
 
-			vim.defer_fn(function()
-				require("colorizer").attach_to_buffer(0)
-			end, 0)
-		end
-	},
-	-- Indent Blankline
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		config = function()
-			require("ibl").setup()
-		end
-	},
-  -- CMP
-	{
+      vim.defer_fn(function()
+        require("colorizer").attach_to_buffer(0)
+      end,0)
+    end
+  },
+  --IndentBlankline
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require("ibl").setup()
+    end
+  },
+  --CMP
+  {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
-		dependencies = {
-			{
-				"L3MON4D3/LuaSnip",
-				dependencies = "rafamadriz/friendly-snippets"
-			},
+    dependencies = {
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets"
+      },
       {
         "windwp/nvim-autopairs",
         opts = {
           fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
+          disable_filetype = {"TelescopePrompt","vim"},
         },
-        config = function(_, opts)
+        config = function(_,opts)
           require("nvim-autopairs").setup(opts)
 
-          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+          local cmp_autopairs = require"nvim-autopairs.completion.cmp"
+          require("cmp").event:on("confirm_done",cmp_autopairs.on_confirm_done())
         end,
       },
       "hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path"
+      "hrsh7th/cmp-path"
     },
     config = function()
       require("pml68.configs.cmp")
     end,
   },
-	-- Terminal
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		opts = {
-			shade_terminals = false,
-			persist_size = false,
-			size = function(term)
-    		if term.direction == "horizontal" then
-      		return 15
-    		elseif term.direction == "vertical" then
-      		return vim.o.columns * 0.4
-    		end
-  		end,
-		},
-		config = function(_, opts)
-			require("pml68.configs.toggleterm")
-			require("toggleterm").setup(opts)
-		end
-	},
-  -- Undotree
+  --Terminal
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      shade_terminals = false,
+      persist_size = false,
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+    },
+    config = function(_,opts)
+      require("pml68.configs.toggleterm")
+      require("toggleterm").setup(opts)
+    end
+  },
+  --Undotree
   {
     "mbbill/undotree",
     cmd = "UndotreeToggle",
   },
-  -- Git
+  --Git
   {
     "tpope/vim-fugitive",
     event = "BufReadPre"
@@ -82,55 +82,55 @@ local plugins = {
     "lewis6991/gitsigns.nvim",
     event = "BufReadPre",
     opts = {
-		signs = {
-    	add = { text = "│" },
-    	change = { text = "│" },
-    	delete = { text = "󰍵" },
-    	topdelete = { text = "‾" },
-    	changedelete = { text = "~" },
-    	untracked = { text = "│" },
-  	},
-		on_attach = function(bufnr)
-	  	local gs = package.loaded.gitsigns
+      signs = {
+        add = {text = "│"},
+        change = {text = "│"},
+        delete = {text = "󰍵"},
+        topdelete = {text = "‾"},
+        changedelete = {text = "~"},
+        untracked = {text = "│"},
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
 
-	  	local function map(mode, l, r, opts)
-	    	opts = opts or {}
-	    	opts.buffer = bufnr
-	    	vim.keymap.set(mode, l, r, opts)
-    	end
+        local function map(mode,l,r,opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode,l,r,opts)
+        end
 
-	  	map("n", "<leader>gb", gs.blame_line)
-		end,
+        map("n","<leader>gb",gs.blame_line)
+      end,
     },
-    config = function(_, opts)
+    config = function(_,opts)
       require("gitsigns").setup(opts)
     end
   },
-  -- LSP
+  --LSP
   {
     "neovim/nvim-lspconfig",
-    config = function ()
-      require "pml68.configs.lspconfig"
+    config = function()
+      require("pml68.configs.lspconfig")
     end
   },
   {
     "williamboman/mason.nvim",
-    cmd = {"Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate"},
+    cmd = {"Mason","MasonInstall","MasonInstallAll","MasonUpdate"},
     opts = function()
       return require("pml68.configs.mason")
     end,
-    config = function(_, opts)
+    config = function(_,opts)
       require("mason").setup(opts)
-      vim.api.nvim_create_user_command("MasonInstallAll", function()
-				if opts.ensure_installed and #opts.ensure_installed > 0 then
-					vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
-				end
-      end, {})
+      vim.api.nvim_create_user_command("MasonInstallAll",function()
+        if opts.ensure_installed and #opts.ensure_installed > 0 then
+          vim.cmd("MasonInstall"..table.concat(opts.ensure_installed,""))
+        end
+      end,{})
 
       vim.g.mason_binaries_list = opts.ensure_installed
     end
   },
-  -- Fuzzy finder
+  --Fuzzyfinder
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.6",
@@ -141,53 +141,53 @@ local plugins = {
       require("pml68.configs.telescope")
     end
   },
-  -- Color scheme
+  --Colorscheme
   {
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function ()
-			require("rose-pine").setup({
-				styles = {
-					transparency = true
-				}
-			})
-			vim.cmd("colorscheme rose-pine")
-		end
-	},
-  -- Syntax highlighting
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        styles = {
+          transparency = true
+        }
+      })
+      vim.cmd("colorscheme rose-pine")
+    end
+  },
+  --Syntaxhighlighting
   {
     "nvim-treesitter/nvim-treesitter",
-    event = {"BufReadPost", "BufNewFile"},
+    event = {"BufReadPost","BufNewFile"},
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
-					"c",
-					"cpp",
-					"lua",
-					"kotlin",
-					"nasm",
-					"bash",
-					"html",
-					"scss",
-					"css",
-					"typescript",
-					"javascript",
-					"svelte",
-					"python",
-					"rust",
-					"markdown",
-					"json",
-					"glsl",
-					"dart",
-				},
-				highlight = {
-					enable = true
-				}
+          "c",
+          "cpp",
+          "lua",
+          "kotlin",
+          "nasm",
+          "bash",
+          "html",
+          "scss",
+          "css",
+          "typescript",
+          "javascript",
+          "svelte",
+          "python",
+          "rust",
+          "markdown",
+          "json",
+          "glsl",
+          "dart",
+        },
+        highlight = {
+          enable = true
+        }
       })
     end
   },
-  -- Misc
+  --Misc
   {
     "andweeb/presence.nvim",
     lazy = false,
@@ -200,14 +200,14 @@ local plugins = {
     "stevearc/dressing.nvim",
     event = "VeryLazy",
   },
-  -- Formatting
+  --Formatting
   {
     "nvimdev/guard.nvim",
     dependencies = {
       "nvimdev/guard-collection"
     },
     event = "BufReadPre",
-    config = function ()
+    config = function()
       local ft = require("guard.filetype")
 
       ft("c,cpp,h"):fmt("clang-format")
@@ -222,12 +222,12 @@ local plugins = {
       })
     end
   },
-  -- Dap
+  --Dap
   {
     "mfussenegger/nvim-dap",
-		init = function()
-			require("pml68.configs.dap")
-		end
+    init = function()
+      require("pml68.configs.dap")
+    end
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
@@ -244,9 +244,9 @@ local plugins = {
     "rcarriga/nvim-dap-ui",
     event = "VeryLazy",
     dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio"
-		},
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
+    },
     config = function()
       local dap = require("dap")
       local dapui = require("dapui")
@@ -263,16 +263,16 @@ local plugins = {
       end
     end,
   },
-  -- Dart/Flutter
+  --Dart/Flutter
   {
     "akinsho/flutter-tools.nvim",
-		ft = "dart",
+    ft = "dart",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim",
     },
   },
-  -- PKL
+  --PKL
   {
     "https://github.com/apple/pkl-neovim",
     lazy = true,
@@ -284,12 +284,12 @@ local plugins = {
       vim.cmd("TSInstall! pkl")
     end
   },
-  -- JSON
+  --JSON
   {
     "b0o/schemastore.nvim",
     ft = "json",
   },
-  -- Rust
+  --Rust
   {
     "rust-lang/rust.vim",
     ft = "rust",
@@ -297,29 +297,29 @@ local plugins = {
       vim.g.rustfmt_autosave = 1
     end,
   },
-	{
-		"saecki/crates.nvim",
-		tag = "stable",
-		dependencies = "hrsh7th/nvim-cmp",
-		event = {"BufRead Cargo.toml"},
-		config = function()
-			local crates = require("crates")
-			crates.setup()
+  {
+    "saecki/crates.nvim",
+    tag = "stable",
+    dependencies = "hrsh7th/nvim-cmp",
+    event = {"BufRead Cargo.toml"},
+    config = function()
+      local crates = require("crates")
+      crates.setup()
 
-			vim.keymap.set("n", "<leader>rcu", function()
-				crates.upgrade_all_crates()
-			end)
-		end
-	},
-	-- LaTeX
-	{
-		"lervag/vimtex",
-		lazy = false,
-		dependencies = "micangl/cmp-vimtex",
-		init = function()
-			vim.g.vimtex_view_method = 'mupdf'
-		end
-	},
+      vim.keymap.set("n","<leader>rcu",function()
+        crates.upgrade_all_crates()
+      end)
+    end
+  },
+  --LaTeX
+  {
+    "lervag/vimtex",
+    lazy = false,
+    dependencies = "micangl/cmp-vimtex",
+    init = function()
+      vim.g.vimtex_view_method = 'mupdf'
+    end
+  },
 }
 
 return plugins
