70b642568d5d6d7a26b756e2daacf0c2ee75dd3d
pml68 <tutorialmester@gmail.com>
2024-04-29 22:52:59 +0200

    refactor: create discrete lua file for vim.o and vim.g settings

--- lua/pml68/init.lua
+++ lua/pml68/init.lua
@@ -1,32 +1,3 @@
+require("pml68.settings")
 require("pml68.remap")
 require("pml68.lazy")
-
-vim.opt.rnu = true
-vim.opt.nu = true
-
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.swapfile = false
-
-vim.opt.scrolloff = 10
-
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-
-vim.opt.clipboard = "unnamedplus"
-
-vim.opt.termguicolors = true
-
-vim.opt.whichwrap:append "<>[]hl"
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
-
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
---
+++ lua/pml68/settings.lua
@@ -0,0 +1,29 @@
+vim.o.rnu = true
+vim.o.nu = true
+
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+
+vim.o.smartindent = true
+
+vim.o.swapfile = false
+
+vim.o.scrolloff = 10
+
+vim.o.hlsearch = true
+vim.o.incsearch = true
+
+vim.o.clipboard = "unnamedplus"
+
+vim.o.termguicolors = true
+
+vim.opt.whichwrap:append "<>[]hl"
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
