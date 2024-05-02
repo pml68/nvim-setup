c4e0acad08b4be4343dc9cf8b783d8f86cf2efe8
pml68 <tutorialmester@gmail.com>
2024-05-02 18:10:42 +0200

    refactor: move around some keymaps

--- lua/pml68/remap.lua
+++ lua/pml68/remap.lua
@@ -1,14 +1,26 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>gB", "<cmd>Git blame<CR>")
-vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>")
 vim.keymap.set("n", "<leader>n", "<cmd>set nu!<CR>")
 vim.keymap.set("n", "<leader>rn", "<cmd>set rnu!<CR>")
+vim.keymap.set("n", "<Esc>", "<cmd>nohl<CR>")
+vim.keymap.set("n", "<leader>x", "<cmd>bd<CR>")
 vim.keymap.set("n", "<leader>fo", "=ap")
+
+-- Switch between windows
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
 
 -- VimTex
 vim.keymap.set("n", "<leader>ll", "<cmd>VimtexCompile<CR>")
 vim.keymap.set("n", "<leader>vl", "<cmd>VimtexView<CR>")
+
+-- Undotree
+vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>")
+
+-- Gitsigns
+vim.keymap.set("n", "<leader>gB", "<cmd>Git blame<CR>")
 
 -- Screenkey
 vim.keymap.set("n", "<leader>sk", "<cmd>Screenkey<CR>")
@@ -25,18 +37,6 @@ vim.keymap.set('n', '<leader>fg', "<cmd>Telescope git_files<CR>", {})
 vim.keymap.set("n", "<leader>h", "<cmd>ToggleTerm direction=horizontal<CR>")
 vim.keymap.set("n", "<leader>v", "<cmd>ToggleTerm direction=vertical<CR>")
 
--- highlights
-vim.keymap.set("n", "<Esc>", "<cmd>nohl<CR>")
-
--- switch between windows
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-
--- close current buffer
-vim.keymap.set("n", "<leader>x", "<cmd>bd<CR>")
-
 -- Diagnostics, LSP
 vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>')
 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
@@ -47,7 +47,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
     local opts = { buffer = event.buf }
 
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.keymap.set('n', '<leader>gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
@@ -55,7 +55,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<CR>', opts)
+    vim.keymap.set({ 'n', 'x' }, '<S-f>', '<cmd>lua vim.lsp.buf.format({async = true})<CR>', opts)
     vim.keymap.set('n', 'gc', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   end
 })
