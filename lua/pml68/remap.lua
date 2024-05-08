f908defe0ab7cd24e1b62632040be96f3fadbd5c
pml68 <tutorialmester@gmail.com>
2024-05-08 23:32:20 +0200

    feat: add 2 new remaps

--- lua/pml68/remap.lua
+++ lua/pml68/remap.lua
@@ -5,6 +5,8 @@ vim.keymap.set("n", "<leader>rn", "<cmd>set rnu!<CR>")
 vim.keymap.set("n", "<Esc>", "<cmd>nohl<CR>")
 vim.keymap.set("n", "<leader>x", "<cmd>bd<CR>")
 vim.keymap.set("n", "<leader>fo", "=ap")
+vim.keymap.set("n", "<leader>b", "<cmd>!chmod 755 %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>w", ":w<CR>:so<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
