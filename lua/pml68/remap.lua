348e14345d65e4b4323be108d35f919e892281eb
pml68 <tutorialmester@gmail.com>
2024-07-17 22:34:19 +0200

    feat: set <Tab> to jump to last window

--- lua/pml68/remap.lua
+++ lua/pml68/remap.lua
@@ -25,6 +25,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<Tab>", "<C-w>w")
 
 -- Switch between panes
 vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>")
