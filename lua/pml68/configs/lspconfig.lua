18b24ca0a27c2e9f88b66ca7664672e178a3e5e3
pml68 <contact@pml68.me>
2024-08-26 00:19:22 +0200

    refactor: use the "official" configuration for termux ls instead of whatever I threw together

--- lua/pml68/configs/lspconfig.lua
+++ lua/pml68/configs/lspconfig.lua
@@ -23,6 +23,17 @@ local servers = {
   "csharp_ls",
 }
 
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "build.sh", "*.subpackage.sh", "PKGBUILD", "*.install",
+    "makepkg.conf", "*.ebuild", "*.eclass", "color.map", "make.conf" },
+  callback = function()
+    vim.lsp.start({
+      name = "termux",
+      cmd = { "termux-language-server" }
+    })
+  end,
+})
+
 for _, server in pairs(servers) do
   local opts = {
     capabilities = capabilities,
--- lua/pml68/configs/settings/bashls.lua
+++
@@ -1,6 +0,0 @@
-vim.lsp.start({
-  cmd = { "termux-language-server" },
-  root_dir = vim.fn.getcwd(),
-})
-
-return {}
