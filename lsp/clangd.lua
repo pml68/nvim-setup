d3614a32ab088207b4d118bc39381a967e7ab831
Polesznyák Márk <contact@pml68.dev>
2025-10-18 03:14:00 +0200

    fix: clangd being started on non-C/C++ filetypes

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,9 +1,10 @@
 return {
+  filetypes = { "c", "cpp", "h" },
   cmd = {
     vim.fn.stdpath('data') .. "/mason/bin/clangd",
     "--header-insertion=never"
   },
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
-  end
+  end,
 }
